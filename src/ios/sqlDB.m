80e5eda68ac5e00ce8f49743185e5dba55412f72
Michal Kuklis <michal.kuklis@gmail.com>
2018-06-07 00:48:16 -0400

    Check for empty source

--- src/ios/sqlDB.m
+++ src/ios/sqlDB.m
@@ -28,12 +28,12 @@
 
 -(void)copyDb:(int)location source:(NSString*)src db:(NSString*)dbname callBackID:(NSString*)cid
 {
-	
+
 	// NSLog(@"[sqlDB] documentsDirectory = %@",documentsDirectory);
 	// NSLog(@"[sqlDB] libraryDirectory = %@",libraryDirectory);
 	NSError *error = nil;
 	fileManager = [NSFileManager defaultManager];
-	
+
 	if (location == 2) {
 		//set Document as default target directory
 		documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
@@ -62,8 +62,8 @@
 			}
 		}
 	}
-	
-	
+
+
 	//Get Database from www directory
 	NSString *sourceDB = @"";
 	if([src rangeOfString:@"www"].location != NSNotFound){
@@ -78,16 +78,16 @@
 			//sourceDB = [src stringByAppendingPathComponent:dbname];
 		}
 	}
-	
+
 	NSLog(@"[sqlDB] Source: %@",sourceDB);
 	NSLog(@"[sqlDB] Destination: %@",dbPath);
-	
+
 	//copy database from www directory to target directory
 	if (!([fileManager copyItemAtPath:sourceDB toPath:dbPath error:&error])) {
 		NSLog(@"[sqlDB] Could not copy file from %@ to %@. Error = %@",sourceDB,dbPath,error);
 		[self sendPluginResponse:error.code msg:error.description err:YES callBackID:cid];
 		//        NSInteger ecode = [error code];
-		
+
 		//        [err setObject:[NSNumber numberWithUnsignedInteger:ecode] forKey:@"code"];
 		//
 		//        [err setObject:error.description forKey:@"message"];
@@ -98,26 +98,26 @@
 		//        result =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"File Copied"];
 		[self sendPluginResponse:200 msg:@"File Copied" err:NO callBackID:cid];
 	}
-	
+
 }
 
 - (void)copy:(CDVInvokedUrlCommand*)command
 {
-	
+
 	// BOOL success;
 	int location = 0;
-	
+
 	//CDVPluginResult *result = nil;
-	
+
 	NSString *dbname = [command argumentAtIndex:0];
 	location = [[command argumentAtIndex:1] intValue];
 	//NSMutableDictionary *err = [NSMutableDictionary dictionaryWithCapacity:2];
-	
+
 	NSLog(@"[sqlDB] Dbname = %@",dbname);
 	NSLog(@"[sqlDB] location = %d",location);
-	
+
 	[self copyDb:location source:@"www" db:dbname callBackID:command.callbackId];
-	
+
 	// [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
@@ -125,15 +125,20 @@
 	NSString *dbname = [command argumentAtIndex:0];
 	NSString *source = [command argumentAtIndex:1];
 	NSString *src;
-	if([source rangeOfString:@"file://"].location != NSNotFound){
+
+	if([source length] == 0){
+		documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+		src = [documentsDirectory stringByAppendingPathComponent:dbname];
+	}
+	else if([source rangeOfString:@"file://"].location != NSNotFound){
 		//sourceDB = [src stringByReplacingOccurrencesOfString:@"file://" withString:@""];
 		src = [[source stringByReplacingOccurrencesOfString:@"file://" withString:@""] stringByAppendingPathComponent:dbname];
 	} else {
 		//sourceDB = src;
 		src = [source stringByAppendingPathComponent:dbname];
 	}
-	
-	if([fileManager fileExistsAtPath:src]) {
+
+    if(([fileManager fileExistsAtPath:src]) == YES){
 		[self sendPluginResponse:200 msg:@"File Exists" err:NO callBackID:command.callbackId];
 	} else {
 		[self sendPluginResponse:400 msg:@"File Doesn't Exists" err:NO callBackID:command.callbackId];
@@ -147,13 +152,13 @@
 	NSString *filename = [command argumentAtIndex:0];
 	location = [[command argumentAtIndex:1] intValue];
 	//CDVPluginResult *result = nil;
-	
+
 	documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-	
+
 	libraryDirectory = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-	
+
 	fileManager = [NSFileManager defaultManager];
-	
+
 	if (location == 2) {
 		dbPath = [documentsDirectory stringByAppendingPathComponent:filename];
 	} else if(location == 1) {
@@ -162,11 +167,11 @@
 		NSString *nosync = [libraryDirectory stringByAppendingPathComponent:@"LocalDatabase"];
 		dbPath = [nosync stringByAppendingPathComponent:filename];
 	}
-	
+
 	NSLog(@"[sqlDB] dbPath for deletion= %@",dbPath);
-	
+
 	// NSMutableDictionary *err = [NSMutableDictionary dictionaryWithCapacity:2];
-	
+
 	if([fileManager fileExistsAtPath:dbPath])
 	{
 		NSError *error = nil;
@@ -186,19 +191,19 @@
 		//result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"File Doesn't Exists"];
 		[self sendPluginResponse:404 msg:@"File Does Not Exists" err:YES callBackID:command.callbackId];
 	}
-	
+
 	//    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 
 -(void) copyDbFromStorage:(CDVInvokedUrlCommand *)command
 {
-	
+
 	// BOOL success;
 	int location = 0;
-	
+
 	//CDVPluginResult *result = nil;
-	
+
 	NSString *dbname = [command argumentAtIndex:0];
 	location = [[command argumentAtIndex:1] intValue];
 	NSString *src = [command argumentAtIndex:2];
@@ -208,21 +213,21 @@
 	NSLog(@"deleteolddb = %d",deleteolddb);
 	if(deleteolddb) {
 		documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-		
+
 		libraryDirectory = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-		
+
 		fileManager = [NSFileManager defaultManager];
-		
+
 		if (location == 0) {
 			NSString *nosync = [libraryDirectory stringByAppendingPathComponent:@"LocalDatabase"];
 			dbPath = [nosync stringByAppendingPathComponent:dbname];
-			
+
 		} else if(location == 1) {
 			dbPath = [libraryDirectory stringByAppendingPathComponent:dbname];
 		} else {
 			dbPath = [documentsDirectory stringByAppendingPathComponent:dbname];
 		}
-		
+
 		if(([fileManager fileExistsAtPath:dbPath]) == YES){
 			if(!([fileManager removeItemAtPath:dbPath error:&error])) {
 				[self sendPluginResponse:error.code msg:error.description err:YES callBackID:command.callbackId];
@@ -241,22 +246,22 @@
 -(void) copyDbToStorage:(CDVInvokedUrlCommand *) command
 {
 	int location = 0;
-	
+
 	//CDVPluginResult *result = nil;
-	
+
 	NSString *dbname = [command argumentAtIndex:0];
 	location = [[command argumentAtIndex:1] intValue];
 	NSString *dest = [command argumentAtIndex:2];
 	BOOL overwrite = [[command argumentAtIndex:3] boolValue];
 	//NSMutableDictionary *err = [NSMutableDictionary dictionaryWithCapacity:2];
-	
+
 	NSLog(@"[sqlDB] Dbname = %@",dbname);
 	NSLog(@"[sqlDB] location = %d",location);
 	// NSLog(@"[sqlDB] documentsDirectory = %@",documentsDirectory);
 	// NSLog(@"[sqlDB] libraryDirectory = %@",libraryDirectory);
 	NSError *error = nil;
 	fileManager = [NSFileManager defaultManager];
-	
+
 	if (location == 2) {
 		//set Document as default target directory
 		documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
@@ -285,13 +290,13 @@
 			}
 		}
 	}
-	
+
 	NSString *destination = dest;
-	
+
 	if([dest rangeOfString:@"file://"].location != NSNotFound){
 		destination = [dest stringByReplacingOccurrencesOfString:@"file://" withString:@""];
 	}
-	
+
 	NSLog(@"[sqlDB] destination = %@",destination);
 //	NSLog(@"[sqlDB] dest = %@",dest);
 	BOOL isDir = YES;
@@ -300,13 +305,13 @@
 	if(!dirExists){
 		[[NSFileManager defaultManager] createDirectoryAtPath:destination withIntermediateDirectories:YES attributes:nil error:&error];
 	}
-	
+
 	destination = [destination stringByAppendingPathComponent:dbname];
-	
+
 	NSLog(@"[sqlDB] Source: %@",dbPath);
 	NSLog(@"[sqlDB] Destination: %@",destination);
 	BOOL copyContinue = YES;
-	
+
 	if([fileManager fileExistsAtPath:destination] == YES) {
 		NSLog(@"Ovwerwrite = %d",overwrite);
 		if(overwrite) {
@@ -322,7 +327,7 @@
 			NSLog(@"[sqlDB] Could not copy file from %@ to %@. Error = %@",dbPath,destination,error);
 			[self sendPluginResponse:error.code msg:error.description err:YES callBackID:command.callbackId];
 			//        NSInteger ecode = [error code];
-			
+
 			//        [err setObject:[NSNumber numberWithUnsignedInteger:ecode] forKey:@"code"];
 			//
 			//        [err setObject:error.description forKey:@"message"];
